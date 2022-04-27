550545f85e91ae00ad411d79bda3a07a19ee5732
Laika Deunert <laikadeunert@MacBook-Air.local>
2022-04-26 23:04:53 -0700

    commit tree b9eb0cbd57f298a7a75a045f11f30e788883f279

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,20 @@
+let state = { friends: [{ name: 'Avi', hometown: 'NYC', id: 100}] };
+let action = {
+  type: "ADD_FRIEND",
+  friend: {
+    name: "Joe",
+    hometown: "Boston",
+    id: 101,
+  },
+};
+
 export function manageFriends(state, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+          return {friends: [...state.friends, action.friend]};
+        case 'REMOVE_FRIEND':
+          return {friends: [...state.friends.filter( friend => friend.id != action.id)]};
+    default:
+       return state;
+    }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,13 @@
+let state = 
+{ type: 'INCREASE', 
+numberOfPresents: 0 }
+
 export function managePresents(state, action){
+    switch(action.type) {
+        case 'INCREASE':
+            return { numberOfPresents: state.numberOfPresents + 1};
+    default:
+        return state
+    }
+
 }
